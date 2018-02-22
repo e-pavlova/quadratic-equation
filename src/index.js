81e5ab9f4607fe3c6400ba7d73c4a6923e300ce5
Ekaterina Pavlova <pavlova.ea.91@gmail.com>
2018-02-23 02:59:54 +0300

    Solve quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function solveEquation(equation) {
+module.exports = function solveEquation(str)
+{
   // your implementation
+    for (var i = 0; i < str.length; i++)
+    {
+        str = str.split(' ').join('');
+    }
+
+    str = str.replace('^2', '');
+
+    var regexp=/(\-\d+|\d+)/g;
+
+    var resArr = str.match(regexp);
+
+    var a = resArr[0];
+    var b = resArr[1];
+    var c = resArr[2];
+
+    var root1 = (- b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    var root2 = (- b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    var rootsArr = [];
+
+    rootsArr.push(Math.round(root1), Math.round(root2));
+
+    rootsArr.sort((x, y) => x - y);
+
+    return rootsArr;
 }
